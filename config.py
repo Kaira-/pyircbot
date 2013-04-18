d6e0a9f788f1e0b6434a747f18adcf5e974e6d37
Jukka Pietilä <jukkapietila@gmail.com>
2013-04-18 23:45:36 +0300

    Made some additions and changes to config.py and confreader.py
    Removed unnecessary __init__-method from config.py and fleshed out
    as well as commented the other constructor
    
    Refactored a bit the code in confreader.py, now has a method for
    seeking value from config-file via internal method instead of
    hardcoding if-checks for finding it.

--- config.py
+++ config.py
@@ -7,9 +7,19 @@ class Config(object):
 		self.BOTNAME = ""
 		self.PORT = 6667
 		self.CHANNELS = []
-		
-	def __init__(self, serv, name, port, channels):
+		self.JOINMSG = "I am here."
+	def __init__(self, serv, name, port, channels, joinmsg):
+		"""
+		Constructs a new Config-object with given parameters.
+		@serv - Servername to connect to
+		@name - The bot's name
+		@port - Port to utilize during establishing connection
+		@channels - A string representation of channels where the bot
+		will join, separated by ','
+		@joinmsg - A message to be sent when the bot joins the channel
+		"""
 		self.SERVER = serv
 		self.BOTNAME = name
 		self.PORT = port
-		self.CHANNELS = list(channels)
+		self.JOINMSG = joinmsg
+		self.CHANNELS = channels.split(",")
--- confreader.py
+++ confreader.py
@@ -9,3 +9,29 @@ import config
 class ConfReader(object):
 	def __init__(self):
 		self._CONFNAME = "config.conf"
+	
+	def readConfig(self):
+		name = self._findValue("NAME")
+		server = self._findValue("SERVER")
+		channels = self._findValue("CHANNELS")
+		port = self._findValue("PORT")
+		joinmsg = self._findValue("JOINMSG")
+		config = Config(server, name, port, channels, joinmsg)
+
+	def _findValue(self, value):
+		"""
+		Attempts to find the seeked value from self._CONFNAME.
+		If the value can not be found, it returns None, otherwise
+		it returns a string representation of the value
+		"""
+		with open(self._CONFNAME) as f:
+			for line in f.readlines():
+				#strip trailing and leading whitespaces
+				str = line.lstrip()
+				#check if the first non-whitespace character in line is comment-character '#'
+				if str[0] == '#':
+					continue
+				if str.startswith(value):
+					val = str.split("=")[2]
+					return val
+		return None
