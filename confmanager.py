0f509f5039bba4cf813d3a5c7a0db629d07dadb2
Jukka Pietila <jukkapietila@gmail.com>
2013-04-23 00:28:25 +0300

    Added writing-function to confmanager.py

--- confmanager.py
+++ confmanager.py
@@ -9,7 +9,11 @@ class ConfManager(object):
 	def __init__(self):
 		self._confreader = confreader.ConfReader()
 		self._config = self._confreader.readConfig()
+		self._confwriter = confwriter.ConfWriter()
 		
 	def readConfig():
 		self._config = self._confreader.readConfig()
 		return self._config
+		
+	def writeConfig():
+		self._confwriter.writeConfig(self._config)
